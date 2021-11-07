6c46e54468d30d47354d85b6467d991d6b12f790
Nicholas Fagner <nicholasfagner@gmail.com>
2021-11-07 13:57:38 -0700

    completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
 
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
+
+// export default Counter;
+
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//     setCount(count + 1);
+//     console.log(count)
+//     setCount(count + 1);
+//     console.log(count)
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
+
+// export default Counter;
+
 function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
